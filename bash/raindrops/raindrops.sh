1dc41e7d268eef646d41abd86e66797ca0eea0a9
yuan-fei <yuanfei_1984@hotmail.com>
2019-08-09 22:37:16 +0800

    format code

--- bash/raindrops/raindrops.sh
+++ bash/raindrops/raindrops.sh
@@ -5,13 +5,13 @@ set -o errexit
 main () {
 	ans=""
 
-	if (( $1%3 == 0 )); then
+	if (( $1 % 3 == 0 )); then
 		ans="${ans}Pling"
 	fi
-	if (( $1%5 == 0 )); then
+	if (( $1 % 5 == 0 )); then
 		ans="${ans}Plang"
 	fi
-	if (( $1%7 == 0 )); then
+	if (( $1 % 7 == 0 )); then
 		ans="${ans}Plong"
 	fi
 	if [[ -z "${ans}" ]]; then
